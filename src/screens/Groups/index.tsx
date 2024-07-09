fd8b8fd8dda8dcdeb0532fac976bfdf134e0bc9d
Lucas Augusto Kepler <lucaskepler991@gmail.com>
2024-07-09 15:08:43 -0300

    last changes

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,20 +1,29 @@
-import { GroupCard } from '@components/GroupCard';
-import { Header } from '@components/Header';
-import { Highlight } from '@components/Highlight';
+import { GroupCard } from "@components/GroupCard";
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import { FlatList } from "react-native";
+import { ListEmpty } from "@components/ListEmpty";
 
-import { Container } from './styles';
+import { Container } from "./styles";
+import { useState } from "react";
 
 export function Groups() {
+  const [groups, setGroups] = useState<String[]>([]);
   return (
     <Container>
       <Header />
-      <Highlight 
-        title="Turmas"
-        subtitle="jogue com sua turma"
+      <Highlight title="Turmas" subtitle="jogue com sua turma" />
+      <FlatList
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
       />
 
-      <GroupCard 
-        title="Galera do Ignite" />
+      <GroupCard title="Galera do Ignite" />
     </Container>
   );
 }
