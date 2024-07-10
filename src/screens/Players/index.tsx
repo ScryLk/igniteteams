1ed09523b2f87fbba0b285ec3614fe71a95c2d94
Lucas Augusto Kepler <lucaskepler991@gmail.com>
2024-07-10 14:36:48 -0300

    last changes

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,10 +1,17 @@
 import { Header } from "@components/Header";
-import { Container, Form } from "./styles";
+import { Filter } from "@components/Filter";
+import { Container, Form, HeaderList, NumbersOfPlayers } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Input } from "@components/Input";
+import { FlatList } from "react-native";
+import { useState } from "react";
+import { PlayerCard } from "@components/PlayerCard";
+import { ListEmpty } from "@components/ListEmpty";
 
 export function Players() {
+  const [team, setTeam] = useState("Time A");
+  const [players, setPlayers] = useState(["Rodrigo", "Diego", "Vini", "Biro"]);
   return (
     <Container>
       <Header showBackButton />
@@ -13,11 +20,42 @@ export function Players() {
         subtitle="Adicione a Galera e separe os times"
       />
       <Form>
-
-      <Input placeholder="Nome da Pessoa" autoCorrect={false}  />
-      <ButtonIcon icon="add" />
+        <Input placeholder="Nome da Pessoa" autoCorrect={false} />
+        <ButtonIcon icon="add" />
       </Form>
-      
+      <HeaderList>
+        <FlatList
+          data={["Time A", "Time B"]}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+
+        <NumbersOfPlayers>{players.length}</NumbersOfPlayers>
+      </HeaderList>
+
+      <FlatList
+        data={players}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <PlayerCard 
+            name={item} 
+            onRemove={() => { }} 
+            />
+        )}
+        ListEmptyComponent={() => (
+          <ListEmpty
+            message="Não há pessoas nesse time."
+          />
+        )}
+        showsHorizontalScrollIndicator={false}
+      />
     </Container>
   );
 }
